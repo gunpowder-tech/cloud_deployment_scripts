d1be24ff7ff3b54c1a25e6f8223d36e1c6fa9620
Adrian Graham <adrian.a.graham@gmail.com>
2022-04-22 16:19:28 -0700

    Separated NVIDIA driver path. Changed default p4 to t4.

--- deployments/gcp/multi-region/main.tf
+++ deployments/gcp/multi-region/main.tf
@@ -19,6 +19,7 @@ resource "google_storage_bucket" "scripts" {
   location      = var.gcp_region
   storage_class = "REGIONAL"
   force_destroy = true
+  uniform_bucket_level_access = true
 }
 
 module "dc" {
